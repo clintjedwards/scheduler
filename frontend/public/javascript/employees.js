4896502c727d52ec581453caafa0780c08cecd8d
Clint J Edwards <clint.j.edwards@gmail.com>
2020-08-22 18:03:04 -0400

    Finished implementation of add and remove unavailabilities

--- frontend/public/javascript/employees.js
+++ frontend/public/javascript/employees.js
@@ -22,6 +22,34 @@ function populatePositionSelector() {
   });
 }
 
+function addUnavailabilityField() {
+  var new_unavail_field = document.createElement("div");
+  new_unavail_field.classList.add("input-field");
+  new_unavail_field.classList.add("suffix");
+
+  var new_input_field = document.createElement("input");
+  new_input_field.id = "icon_prefix";
+  new_input_field.type = "text";
+
+  var new_icon_field = document.createElement("i");
+  new_icon_field.classList.add("material-icons");
+  new_icon_field.classList.add("clickable");
+  new_icon_field.innerText = "clear";
+  new_icon_field.addEventListener("click", function (e) {
+    removeUnavailabilityField(e.target);
+  });
+
+  new_unavail_field.appendChild(new_input_field);
+  new_unavail_field.appendChild(new_icon_field);
+
+  let list = document.getElementById("unavailability-list");
+  list.appendChild(new_unavail_field);
+}
+
+function removeUnavailabilityField(e) {
+  e.parentNode.remove();
+}
+
 document.addEventListener("DOMContentLoaded", function () {
   renderEmployees();
 
@@ -37,6 +65,11 @@ document.addEventListener("DOMContentLoaded", function () {
   add_button.addEventListener("click", function (e) {
     var instance = M.Modal.getInstance(add_modal);
     instance.open();
+  });
+
+  let add_unavailability_button = document.querySelector("#add-unavailability");
+  add_unavailability_button.addEventListener("click", function (e) {
+    addUnavailabilityField();
   });
 
   populatePositionSelector();
