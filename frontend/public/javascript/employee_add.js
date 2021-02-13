3e91cb9265403af2a7af86a6f45dd5780a847857
Clint J Edwards <clint.j.edwards@gmail.com>
2021-02-13 05:27:26 -0500

    Add unavail button functionality

--- frontend/public/javascript/employee_add.js
+++ frontend/public/javascript/employee_add.js
@@ -38,7 +38,46 @@ function help_collapse() {
   });
 }
 
+function add_unavail_field(count) {
+  let times = document.getElementById("unavail_times");
+
+  let new_field = document.createElement("input");
+  new_field.setAttribute("type", "text");
+  new_field.setAttribute("autocomplete", "off");
+  new_field.setAttribute("id", "time-" + count);
+  new_field.setAttribute(
+    "class",
+    "mb-2 text-2xl font-light border border-transparent mt-1 text-center shadow-sm border-gray-500 rounded-md"
+  );
+
+  times.appendChild(new_field);
+  return count + 1;
+}
+
+function remove_unavail_field(count) {
+  let times = document.getElementById("unavail_times");
+
+  times.removeChild(times.lastChild);
+  return count - 1;
+}
+
+function add_unavail_button_actions() {
+  var count = 0;
+
+  let add_button = document.getElementById("add-button");
+  let remove_button = document.getElementById("remove-button");
+
+  add_button.addEventListener("click", function() {
+    count = add_unavail_field(count);
+  });
+
+  remove_button.addEventListener("click", function() {
+    count = remove_unavail_field(count);
+  });
+}
+
 document.addEventListener("DOMContentLoaded", function() {
   populatePositions();
   help_collapse();
+  add_unavail_button_actions();
 });
